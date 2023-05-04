1b4712a2e7a4fec21c5f27330b166f3f2d6b3ac9
nidhi <nidhi7485@gmail.com>
2023-05-04 19:32:34 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import PrivateRoute from './PrivateRoute'
+import SingleProduct from './SingleProductPage'
+import Checkout from './CheckoutPage'
+
+export {
+  Home,
+  About,
+  Cart,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+  Checkout,
+}
